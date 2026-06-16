a5630364e3ac93d03c7358cad98f4e82d50c616a
ShanedalyCS <shanedaly0903@protonmail.com>
2026-06-16 20:15:18 +0100

    individual group pages infra

--- app/routes/individualGroup.tsx
+++ app/routes/individualGroup.tsx
@@ -1,16 +1,9 @@
-import { Button } from "../components/ui/button";
-
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../components/ui/table";
-
 import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Header from "~/components/header";
+import { Button } from "~/components/ui/button";
+import "../custom.css";
+import { Table } from "lucide-react";
 
 type Group = {
   id: number;
@@ -22,59 +15,58 @@ type Group = {
   departureDate: string;
 };
 
-export default function Groups() {
-  const [groupTable, setGroups] = useState<Group[]>([]);
+export default function IndividualGroup() {
+  const { groupId } = useParams();
+  const [group, setGroup] = useState<Group | null>(null);
 
   useEffect(() => {
-    async function fetchGroups() {
-      try {
-        const response = await fetch("http://localhost:8080/groups");
-        const data = await response.json();
+    if (!groupId) return;
 
-        setGroups(data);
+    async function fetchGroup() {
+      try {
+        const response = await fetch(`http://localhost:8080/groups/${groupId}`);
+        const data = await response.json();
+        setGroup(data);
+        console.log("Fetched group:", data);
       } catch (error) {
-        console.error("Failed to fetch groups:", error);
+        console.error("Failed to fetch group:", error);
       }
     }
-
-    fetchGroups();
-  }, []);
+    fetchGroup();
+  }, [groupId]);
 
   return (
-    <>
-      <h1>Groups</h1>
+  <>
+    <Header />
+    <a href="/groups">Back</a>
+    <div className="individual-group-container">
+      <h1 className="individual-group-title">{group?.name} ({group?.id})</h1>
+      <p>Center: {group?.center}</p>
+      <p>Number of Group Leaders: {group?.numGroupLeaders}</p>
+      <p>Number of Students: {group?.numStudents}</p>
+      <p>Arrival Date: {group?.arrivalDate}</p>
+      <p>Departure Date: {group?.departureDate}</p>
 
-      <Button>This is a button</Button>
-
-      <Table>
-        <TableCaption>A list of your groups.</TableCaption>
-
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Group ID</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Center</TableHead>
-            <TableHead>Num. GL</TableHead>
-            <TableHead>Num. Students</TableHead>
-            <TableHead>Arrival Date</TableHead>
-            <TableHead>Departure Date</TableHead>
-          </TableRow>
-        </TableHeader>
-
-        <TableBody>
-          {groupTable.map((group) => (
-            <TableRow key={group.id}>
-              <TableCell className="font-medium">{group.id}</TableCell>
-              <TableCell>{group.name}</TableCell>
-              <TableCell>{group.center}</TableCell>
-              <TableCell>{group.numGroupLeaders}</TableCell>
-              <TableCell>{group.numStudents}</TableCell>
-              <TableCell>{group.arrivalDate}</TableCell>
-              <TableCell>{group.departureDate}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </>
+      <table>
+        <tr>
+          <th>Group Leader ID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Phone Number</th>
+        </tr>
+      </table>
+      
+      <table>
+        <tr>
+          <th>Student ID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th></th>
+        </tr>
+      </table>
+    </div>
+    
+  </>
   );
 }
+
